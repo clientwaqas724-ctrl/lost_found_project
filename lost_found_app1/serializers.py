237929b175319fe2c3e1f1fa1673df374aab08fb
clientwaqas724-ctrl <clientwaqas724@gmail.com>
2025-12-09 17:17:02 +0500

    Update serializers.py

--- lost_found_app1/serializers.py
+++ lost_found_app1/serializers.py
@@ -395,13 +395,13 @@ class UserItemsSerializer(serializers.Serializer):
 
 ###################################################################################################################################################################################################
 class ClaimSerializer(serializers.ModelSerializer):
-    # Map frontend params to model fields
+    # Map frontend params
     foundItem = serializers.CharField(source='found_item.id', write_only=True)
     claimDescription = serializers.CharField(source='claim_description')
     proofOfOwnership = serializers.CharField(source='proof_of_ownership')
-    supportingImages = serializers.CharField(source='supporting_images', required=False, allow_null=True, allow_blank=True)
+    supportingImages = serializers.CharField(source='supporting_images', required=False, allow_blank=True, allow_null=True)
     status = serializers.CharField(default='pending')
-    adminNotes = serializers.CharField(source='admin_notes', required=False, allow_null=True, allow_blank=True)
+    adminNotes = serializers.CharField(source='admin_notes', required=False, allow_blank=True, allow_null=True)
 
     class Meta:
         model = Claim
@@ -432,6 +432,18 @@ class ClaimSerializer(serializers.ModelSerializer):
             admin_notes=validated_data.get('admin_notes', '')
         )
         return claim
+
+    def update(self, instance, validated_data):
+        # Forcefully update fields without validation errors
+        found_item_id = validated_data.get('found_item', {}).get('id', instance.found_item_id)
+        instance.found_item_id = found_item_id
+        instance.claim_description = validated_data.get('claim_description', instance.claim_description)
+        instance.proof_of_ownership = validated_data.get('proof_of_ownership', instance.proof_of_ownership)
+        instance.supporting_images = validated_data.get('supporting_images', instance.supporting_images)
+        instance.status = validated_data.get('status', instance.status)
+        instance.admin_notes = validated_data.get('admin_notes', instance.admin_notes)
+        instance.save()
+        return instance
 ###################################################################################################################################################################################################
 class MessageSerializer(serializers.ModelSerializer):
     sender_info = serializers.SerializerMethodField()
@@ -549,6 +561,7 @@ class AdminDashboardStatsSerializer(DashboardStatsSerializer):
     returned_items = serializers.IntegerField()
     claimed_items = serializers.IntegerField()
     user_registrations_today = serializers.IntegerField()
+
 
 
 
